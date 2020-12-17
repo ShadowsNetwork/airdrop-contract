ad83ba202782854ca82b54877a2f8e019036e26a
lmc <517288724@qq.com>
2020-12-17 09:51:35 +0800

    add lock contract

---
+++ Lock.sol
@@ -0,0 +1,24 @@
+pragma solidity 0.5.15;
+
+contract Lock {
+    // address owner;
+    // address unlockTime;
+    constructor (address owner, uint256 unlockTime) public payable {
+        assembly {
+            sstore(0x00, owner)
+            sstore(0x01, unlockTime)
+        }
+    }
+
+    // Withdraw function once timestamp has passed unlock time
+    function () external payable {
+        assembly {
+            switch gt(timestamp, sload(0x01))
+            case 0 { revert(0, 0) }
+            case 1 {
+                switch call(gas, sload(0x00), balance(address), 0, 0, 0, 0)
+                case 0 { revert(0, 0) }
+            }
+        }
+    }
+}
